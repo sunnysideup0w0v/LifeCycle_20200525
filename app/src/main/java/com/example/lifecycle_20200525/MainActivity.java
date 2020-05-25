dbd513560dce67f4448b75504cfd5e53cd5ae59b
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-25 14:55:25 +0900

    버튼을 추가하고, 두 번째 액티비티로 이동.

--- app/src/main/java/com/example/lifecycle_20200525/MainActivity.java
+++ app/src/main/java/com/example/lifecycle_20200525/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.lifecycle_20200525;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import com.example.lifecycle_20200525.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         setupEvents();
         setValues();
 
@@ -19,7 +24,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-        
+        binding.moveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
     @Override
